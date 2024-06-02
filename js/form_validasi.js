466172d71de83d28021d9ed4d8b33e5e750a3c12
nabiilah9 <160511846+nabiilah9@users.noreply.github.com>
2024-06-02 13:25:56 +0700

    Update form_validasi.js

--- js/form_validasi.js
+++ js/form_validasi.js
@@ -90,7 +90,7 @@ function submited() {
 }
 // fungsi untuk mengecek tahun 2024 keatas
 function validasi_tgl(){
-			event.preventDefault()
-			regex = /^([2][0-9][2][4-9])|([2][1-9][0-9][0-9])|([3-9][0-9][0-9][0-9])(\-)([0][1-9]|[1][1-2])(\-)([0-2][0-9]|[3][0-1])/g // 2024 - 9999
-			console.log(regex.test(inp.value))
-		}
+	event.preventDefault()
+	regex = /^([2][0-9][2][4-9])|([2][1-9][0-9][0-9])|([3-9][0-9][0-9][0-9])(\-)([0][1-9]|[1][1-2])(\-)([0-2][0-9]|[3][0-1])/g // 2024 - 9999
+	console.log(regex.test(inp.value))
+}
